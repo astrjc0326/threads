8e6b05d95a5ff6490237550e922d4d33624e7d3e
Anny Wang <astrjc.anny@gmail.com>
2023-04-19 16:43:14 -0700

    setup for firebase functions deploy

--- funtions/apis/heroku.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-const BASE_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/rfp';
-const CONFIG = { headers: { Authorization: process.env.GIT_TOKEN } };
-
-module.exports = axios.create({
-  baseURL: BASE_URL,
-  headers: CONFIG.headers,
-});
